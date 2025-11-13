4d0b5bb39feef2e78c3a4d06bfa22d918c0131ea
SRAVAN KODARI <sravankodari4@gmail.com>
2025-11-13 15:00:09 +0530

    Rename import from InteractiveProjects to Projects

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Skills from '@/components/Skills';
-import InteractiveProjects from '@/components/Projects';
+import Projects from '@/components/InteractiveProjects';
 import Experience from '@/components/Experience';
 import Education from '@/components/Education';
 import Contact from '@/components/Contact';
